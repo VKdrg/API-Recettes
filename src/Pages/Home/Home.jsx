dba7328fefcc5d9d2d1b5c7f0067db6829f324b8
Viktor G. <viktor.gouchet@gmail.com>
2026-02-10 16:46:19 +0100

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
 import { Header } from "../../Components/Header/Header"
 
 export const Home = () => {
+
+    const [loading, setLoading] = useState(false)
+    const [recipe, setRecipe] = useState([])
+    const [search, setSearch] = useState('')
+    const [filter, setFilter] = useState('')
+
+    useEffect(()=>{
+        setLoading(true)
+        fetch('')
+    })
+    
+
     return(
-        Header
+        <div id="wrapper">
+            <h1>API Recettes</h1>
+            {/**
+             * searchbar 
+             * filters
+             * expr ternaire loading : recipelist
+            */}
+        </div>
     )
 }
