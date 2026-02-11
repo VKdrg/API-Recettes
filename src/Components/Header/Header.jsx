bc545f33a919c67d9b24f0c4da8247a9c55ed87b
Viktor G. <viktor.gouchet@gmail.com>
2026-02-11 15:48:53 +0100

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import { FaHeart } from "react-icons/fa"
-import { NavLink } from "react-router"
+import { Link, NavLink } from "react-router"
 import { useRecipe } from "../../hooks/UseRecipe"
 
 export const Header = () => {
     const { favorites } = useRecipe()
     return (
-        <div>
-            <img src="src/assets/logoTMDB.svg" alt="" />
-            <nav>
-                <NavLink to={'/favorites'} ><FaHeart size={80} />Favorites : {favorites.length} </NavLink>
-            </nav>
-        </div>
+
+        <nav>
+            <Link to={'/'}>
+                <img src="src/assets/logo.svg" alt="website logo" />
+            </Link>
+            <NavLink to={'/favorites'} ><FaHeart size={80} />Favorites : {favorites.length} </NavLink>
+        </nav>
+
     )
 }
